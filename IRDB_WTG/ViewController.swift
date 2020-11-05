55e978e3f301320305a619c5ee79ce01c0648ee6
Will Gibbs <wtg17a@acu.edu>
2020-11-05 14:11:38 -0600

    Initial commit

---
+++ IRDB_WTG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRDB_WTG
+//
+//  Created by William Gibbs on 11/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
